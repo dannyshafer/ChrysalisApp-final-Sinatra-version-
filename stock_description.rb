69469b111bede9ebaf3e93dff1db389d6a5ebd22
dannyshafer <github@danielshafer.com>
2015-08-10 14:31:06 -0700

    update stock scrape

--- stock_description.rb
+++ stock_description.rb
@@ -1,9 +1,14 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pp'
 
 symbol = "aapl"
 url = "http://finance.yahoo.com/q/pr?s=" + symbol +"+Profile"
+url2 = "http://finance.yahoo.com/q?s="+ symbol
 doc = Nokogiri::HTML(open(url))
+doc2 = Nokogiri::HTML(open(url2))
 
-p doc.css('p')[1].text
+pp doc.css('p')[1].text
+
+pp doc2.css('.yfnc_tabledata1')[5].text
 
